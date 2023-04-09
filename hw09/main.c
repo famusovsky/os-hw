168c2aa418d66c086c5e1867e46489a8b8a4c1bd
Alexey Stepanov <st3p-by-step@ya.ru>
2023-04-09 23:14:36 +0300

    hw09 update

--- hw09/main.c
+++ hw09/main.c
@@ -1,12 +1,3 @@
-/*
-Реализовать для родительского и дочернего процесса двухстороннюю связь с
-использованием только одного неименованного канала (pipe) и циклической
-организацией обмена посредством семафора. При этом канал должен быть постоянно
-открытым. Обмен должен завершаться после поочередной передачи в каждую сторону
-по десять сообщений. По завершении обмена обеспечить корректное закрытие каналов
-и удаление семафора.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,7 +8,7 @@
 
 const int MSGS_COUNT = 10;
 const int BUFSIZE = 256;
-#define SEM_KEY 7777
+#define SEM_KEY 1918
 
 int getSemaphore() {
   int sem_id = semget(SEM_KEY, 2, IPC_CREAT | 0666);
@@ -25,8 +16,16 @@ int getSemaphore() {
     perror("Error creating semaphore: ");
     exit(EXIT_FAILURE);
   }
-  semctl(sem_id, 0, SETVAL, 1);
-  semctl(sem_id, 1, SETVAL, 0);
+  int rc = semctl(sem_id, 0, SETVAL, 0);
+  if (rc < 0) {
+      perror("Error initializing semaphore 0: ");
+      exit(EXIT_FAILURE);
+  }
+  rc = semctl(sem_id, 1, SETVAL, 1);
+  if (rc < 0) {
+      perror("Error initializing semaphore 1: ");
+      exit(EXIT_FAILURE);
+  }
 
   printf("Semaphore created with id: %d\n", sem_id);
   return sem_id;
@@ -37,57 +36,61 @@ void eraseSemaphore(int sem_id) {
     perror("Error removing semaphore: ");
     exit(EXIT_FAILURE);
   }
-  if (semctl(sem_id, 1, IPC_RMID, NULL) < 0) {
-    perror("Error removing semaphore: ");
+}
+
+void runSemOp(int sem_id, int sem_num, int sem_op, int sem_flg) {
+  struct sembuf sb;
+  sb.sem_num = sem_num;
+  sb.sem_op = sem_op;
+  sb.sem_flg = sem_flg;
+  if (semop(sem_id, &sb, 1) < 0) {
+    perror("Error running semop: ");
     exit(EXIT_FAILURE);
   }
 }
 
 void parent_process(int fd, int sem_id) {
   char buffer[BUFSIZE];
-  printf("Parent process started\n");
+  printf("Parent: ");
   for (int i = 0; i < MSGS_COUNT; ++i) {
-    semctl(sem_id, 0, SETVAL, 0);
+    runSemOp(sem_id, 0, 0, SEM_UNDO);
 
     printf("Parent: ");
     snprintf(buffer, BUFSIZE, "Message %d from parent", i + 1);
     write(fd, buffer, BUFSIZE);
-    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 1},
-          1);
-    semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 1}, 1);
+    runSemOp(sem_id, 0, 1, 0);
+    runSemOp(sem_id, 1, -1, 0);
 
-    semctl(sem_id, 1, SETVAL, 0);
+    runSemOp(sem_id, 0, 0, SEM_UNDO);
 
     read(fd, buffer, BUFSIZE);
-    printf("Received: %s\n", buffer);
-    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 1},
-          1);
-    semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 1}, 1);
+    printf("Parent: Received: %s\n", buffer);
+    runSemOp(sem_id, 0, 1, 0);
+    runSemOp(sem_id, 1, -1, 0);
   }
 }
 
 void child_process(int fd, int sem_id) {
   char buffer[BUFSIZE];
-  printf("Child process started\n");
+  printf("Child: ");
   for (int i = 0; i < MSGS_COUNT; ++i) {
-    semctl(sem_id, 1, SETVAL, 0);
+    runSemOp(sem_id, 1, 0, SEM_UNDO);
+
+    read(fd, buffer, BUFSIZE);
+    printf("Child: Received: %s\n", buffer);
+    runSemOp(sem_id, 1, 1, 0);
+    runSemOp(sem_id, 0, -1, 0);
+    
+    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = 0, .sem_flg = SEM_UNDO}, 1);
 
     printf("Child: ");
     snprintf(buffer, BUFSIZE, "Message %d from child", i + 1);
     write(fd, buffer, BUFSIZE);
-    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 1},
-          1);
-    semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 1}, 1);
-
-    semctl(sem_id, 0, SETVAL, 0);
-
-    read(fd, buffer, BUFSIZE);
-    printf("Received: %s\n", buffer);
-    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 1},
-          1);
-    semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 1}, 1);
+    runSemOp(sem_id, 1, 1, 0);
+    runSemOp(sem_id, 0, -1, 0);
   }
 }
+
 
 int main() {
   int fd[2];
@@ -99,6 +102,8 @@ int main() {
   }
 
   sem_id = getSemaphore();
+  semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 0, .sem_flg = 0}, 1);
+  semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = 1, .sem_flg = 0}, 1);
 
   pid_t pid;
   if ((pid = fork()) < 0) {
