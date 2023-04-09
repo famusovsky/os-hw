45dfa2912b0de1f268b800003603e6d9aa353d6c
Alexey Stepanov <st3p-by-step@ya.ru>
2023-04-09 23:27:53 +0300

    hw09 update

--- hw09/main.c
+++ hw09/main.c
@@ -10,7 +10,7 @@ const int MSGS_COUNT = 10;
 const int BUFSIZE = 256;
 #define SEM_KEY 1918
 
-int getSemaphore() {
+int getSemaphorePair() {
   int sem_id = semget(SEM_KEY, 2, IPC_CREAT | 0666);
   if (sem_id < 0) {
     perror("Error creating semaphore: ");
@@ -18,13 +18,13 @@ int getSemaphore() {
   }
   int rc = semctl(sem_id, 0, SETVAL, 0);
   if (rc < 0) {
-      perror("Error initializing semaphore 0: ");
-      exit(EXIT_FAILURE);
+    perror("Error initializing semaphore 0: ");
+    exit(EXIT_FAILURE);
   }
   rc = semctl(sem_id, 1, SETVAL, 1);
   if (rc < 0) {
-      perror("Error initializing semaphore 1: ");
-      exit(EXIT_FAILURE);
+    perror("Error initializing semaphore 1: ");
+    exit(EXIT_FAILURE);
   }
 
   printf("Semaphore created with id: %d\n", sem_id);
@@ -32,6 +32,8 @@ int getSemaphore() {
 }
 
 void eraseSemaphore(int sem_id) {
+  printf("Removing semaphore with id: %d\n", sem_id);
+
   if (semctl(sem_id, 0, IPC_RMID, NULL) < 0) {
     perror("Error removing semaphore: ");
     exit(EXIT_FAILURE);
@@ -51,13 +53,12 @@ void runSemOp(int sem_id, int sem_num, int sem_op, int sem_flg) {
 
 void parent_process(int fd, int sem_id) {
   char buffer[BUFSIZE];
-  printf("Parent: ");
   for (int i = 0; i < MSGS_COUNT; ++i) {
     runSemOp(sem_id, 0, 0, SEM_UNDO);
 
-    printf("Parent: ");
     snprintf(buffer, BUFSIZE, "Message %d from parent", i + 1);
     write(fd, buffer, BUFSIZE);
+    printf("Parent: Sent: %s\n", buffer);
     runSemOp(sem_id, 0, 1, 0);
     runSemOp(sem_id, 1, -1, 0);
 
@@ -65,32 +66,26 @@ void parent_process(int fd, int sem_id) {
 
     read(fd, buffer, BUFSIZE);
     printf("Parent: Received: %s\n", buffer);
-    runSemOp(sem_id, 0, 1, 0);
-    runSemOp(sem_id, 1, -1, 0);
   }
+
+  eraseSemaphore(sem_id);
 }
 
 void child_process(int fd, int sem_id) {
   char buffer[BUFSIZE];
-  printf("Child: ");
   for (int i = 0; i < MSGS_COUNT; ++i) {
     runSemOp(sem_id, 1, 0, SEM_UNDO);
 
     read(fd, buffer, BUFSIZE);
     printf("Child: Received: %s\n", buffer);
-    runSemOp(sem_id, 1, 1, 0);
-    runSemOp(sem_id, 0, -1, 0);
-    
-    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = 0, .sem_flg = SEM_UNDO}, 1);
 
-    printf("Child: ");
     snprintf(buffer, BUFSIZE, "Message %d from child", i + 1);
     write(fd, buffer, BUFSIZE);
+    printf("Child: Sent: %s\n", buffer);
     runSemOp(sem_id, 1, 1, 0);
     runSemOp(sem_id, 0, -1, 0);
   }
 }
-
 
 int main() {
   int fd[2];
@@ -101,9 +96,7 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
-  sem_id = getSemaphore();
-  semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 0, .sem_flg = 0}, 1);
-  semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = 1, .sem_flg = 0}, 1);
+  sem_id = getSemaphorePair();
 
   pid_t pid;
   if ((pid = fork()) < 0) {
@@ -119,8 +112,6 @@ int main() {
   close(fd[0]);
   parent_process(fd[1], sem_id);
   close(fd[1]);
-
-  eraseSemaphore(sem_id);
 
   return 0;
 }
