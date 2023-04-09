df3b93f429fcbf504e8555cf0d0c8018eaf7f730
Alexey Stepanov <st3p-by-step@ya.ru>
2023-04-09 22:35:58 +0300

    hw09 update

--- hw09/main.c
+++ hw09/main.c
@@ -3,11 +3,10 @@
 использованием только одного неименованного канала (pipe) и циклической
 организацией обмена посредством семафора. При этом канал должен быть постоянно
 открытым. Обмен должен завершаться после поочередной передачи в каждую сторону
-по десять сообщений.
-По завершении обмена обеспечить корректное закрытие каналов и удаление семафора.
+по десять сообщений. По завершении обмена обеспечить корректное закрытие каналов
+и удаление семафора.
 */
 
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,7 +17,7 @@
 
 const int MSGS_COUNT = 10;
 const int BUFSIZE = 256;
-#define SEM_KEY 1234
+#define SEM_KEY 7777
 
 int getSemaphore() {
   int sem_id = semget(SEM_KEY, 2, IPC_CREAT | 0666);
@@ -26,8 +25,9 @@ int getSemaphore() {
     perror("Error creating semaphore: ");
     exit(EXIT_FAILURE);
   }
-  semctl(sem_id, 0, SETVAL, 0);
+  semctl(sem_id, 0, SETVAL, 1);
   semctl(sem_id, 1, SETVAL, 0);
+
   printf("Semaphore created with id: %d\n", sem_id);
   return sem_id;
 }
@@ -37,46 +37,55 @@ void eraseSemaphore(int sem_id) {
     perror("Error removing semaphore: ");
     exit(EXIT_FAILURE);
   }
+  if (semctl(sem_id, 1, IPC_RMID, NULL) < 0) {
+    perror("Error removing semaphore: ");
+    exit(EXIT_FAILURE);
+  }
 }
 
 void parent_process(int fd, int sem_id) {
   char buffer[BUFSIZE];
+  printf("Parent process started\n");
   for (int i = 0; i < MSGS_COUNT; ++i) {
     semctl(sem_id, 0, SETVAL, 0);
+
     printf("Parent: ");
-    strncpy(buffer, "Message %d from parent", BUFSIZE);
     snprintf(buffer, BUFSIZE, "Message %d from parent", i + 1);
     write(fd, buffer, BUFSIZE);
-
-    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 1}, 1);
+    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 1},
+          1);
     semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 1}, 1);
+
+    semctl(sem_id, 1, SETVAL, 0);
 
     read(fd, buffer, BUFSIZE);
     printf("Received: %s\n", buffer);
-
-    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 1}, 1);
+    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 1},
+          1);
     semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 1}, 1);
   }
 }
 
 void child_process(int fd, int sem_id) {
   char buffer[BUFSIZE];
+  printf("Child process started\n");
   for (int i = 0; i < MSGS_COUNT; ++i) {
     semctl(sem_id, 1, SETVAL, 0);
 
-    semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = -1, .sem_flg = 0}, 1);
-    read(fd, buffer, BUFSIZE);
-    printf("Child: Received: %s\n", buffer);
-    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = 1, .sem_flg = 0}, 1);
+    printf("Child: ");
+    snprintf(buffer, BUFSIZE, "Message %d from child", i + 1);
+    write(fd, buffer, BUFSIZE);
+    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 1},
+          1);
+    semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 1}, 1);
 
     semctl(sem_id, 0, SETVAL, 0);
 
-    printf("Child: ");
-    strncpy(buffer, "Message %d from child", BUFSIZE);
-    snprintf(buffer, BUFSIZE, "Message %d from child", i + 1);
-    write(fd, buffer, strlen(buffer) + 1);
-    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 0}, 1);
-    semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 0}, 1);
+    read(fd, buffer, BUFSIZE);
+    printf("Received: %s\n", buffer);
+    semop(sem_id, &(struct sembuf){.sem_num = 1, .sem_op = -1, .sem_flg = 1},
+          1);
+    semop(sem_id, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 1}, 1);
   }
 }
 
